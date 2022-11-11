c88acb8e03e51de3fa5f893a6b97eb51ab84cd43
qjsss <1908140541@qq.com>
2022-11-12 01:09:38 +0800

    增加射线检测

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -44,59 +44,59 @@ public class MazeGenerator : MonoBehaviour
     Astar astar=new Astar();
     //astar
     bool drawPath=false;
-    public class PriorityQueue
-    {
-        int Compare(MyTuple x1,MyTuple x2)
-        {
-            return x1.x-x2.x;
-        }
-        public MyTuple[]heap;
-        public int size{get;private set;}
-        public PriorityQueue():this(200){}
-        // public PriorityQueue(int capacity):this(capacity,null){}
-        // public PriorityQueue(IComparer<T>comparer):this(200,null){}
-        // public PriorityQueue(int capacity,IComparer<T>comparer)
-        // {
-        //     heap=new T[capacity+1];
-        //     size=0;
-        //     this.comparer=(comparer==null)?Comparer<T>.Default:comparer;
-        // }
-        public PriorityQueue(int capacity)
-        {
-            heap=new MyTuple[capacity+1];
-            size=0;
-        }
-        public void push(MyTuple x)
-        {
-            if(size+1>heap.Length)Array.Resize(ref heap,heap.Length<<1+1);
-            int i=++size;
-            for(;i!=1&&Compare(x,heap[i>>1])<0;i>>=1)
-            {
-                heap[i]=heap[i>>1];
-            }
-            heap[i]=x;
-        }
-        public MyTuple pop()
-        {
-            if(size==0)throw new InvalidOperationException("优先队列为空");
-            MyTuple res=heap[1];
-            MyTuple t=heap[size--];
-            int p=1;
-            for(int s;p<<1<=size;p=s)
-            {
-                s=p<<1;
-                if(s+1<=size&&Compare(heap[s],heap[s+1])>0)s++;
-                if(Compare(t,heap[s])<0)break;
-                heap[p]=heap[s];
-            }
-            heap[p]=t;
-            return res;
-        }   
-        public MyTuple top()
-        {
-            return heap[1];
-        }
-    }
+    // public class PriorityQueue
+    // {
+    //     int Compare(MyTuple x1,MyTuple x2)
+    //     {
+    //         return x1.x-x2.x;
+    //     }
+    //     public MyTuple[]heap;
+    //     public int size{get;private set;}
+    //     public PriorityQueue():this(200){}
+    //     // public PriorityQueue(int capacity):this(capacity,null){}
+    //     // public PriorityQueue(IComparer<T>comparer):this(200,null){}
+    //     // public PriorityQueue(int capacity,IComparer<T>comparer)
+    //     // {
+    //     //     heap=new T[capacity+1];
+    //     //     size=0;
+    //     //     this.comparer=(comparer==null)?Comparer<T>.Default:comparer;
+    //     // }
+    //     public PriorityQueue(int capacity)
+    //     {
+    //         heap=new MyTuple[capacity+1];
+    //         size=0;
+    //     }
+    //     public void push(MyTuple x)
+    //     {
+    //         if(size+1>heap.Length)Array.Resize(ref heap,heap.Length<<1+1);
+    //         int i=++size;
+    //         for(;i!=1&&Compare(x,heap[i>>1])<0;i>>=1)
+    //         {
+    //             heap[i]=heap[i>>1];
+    //         }
+    //         heap[i]=x;
+    //     }
+    //     public MyTuple pop()
+    //     {
+    //         if(size==0)throw new InvalidOperationException("优先队列为空");
+    //         MyTuple res=heap[1];
+    //         MyTuple t=heap[size--];
+    //         int p=1;
+    //         for(int s;p<<1<=size;p=s)
+    //         {
+    //             s=p<<1;
+    //             if(s+1<=size&&Compare(heap[s],heap[s+1])>0)s++;
+    //             if(Compare(t,heap[s])<0)break;
+    //             heap[p]=heap[s];
+    //         }
+    //         heap[p]=t;
+    //         return res;
+    //     }   
+    //     public MyTuple top()
+    //     {
+    //         return heap[1];
+    //     }
+    // }
     // public class MyTupleCompare : IComparer<MyTuple>
     // {
     //     public int Compare(MyTuple x, MyTuple y)
---
+++ Assets/Scripts/RayManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayManager : MonoBehaviour
+{
+    public RaycastHit hitInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.position);
+            }
+        }
+    }
+}
