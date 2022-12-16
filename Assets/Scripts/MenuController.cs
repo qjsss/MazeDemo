b40609c5316ca7cd1cee8e456db2809a069149cb
qjsss <1908140541@qq.com>
2022-12-17 01:49:43 +0800

    no

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public  GameObject escMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // escMenu=GameObject.Find("Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(escMenu.activeInHierarchy)
+                escMenu.SetActive(false);
+            else
+                escMenu.SetActive(true);
+
+        }
+    }
+    public void OnEscButtonClick()
+    {
+        Application.Quit();
+    }
+    public void OnReturnButtonClick()
+    {
+        escMenu.SetActive(false);
+    }
+    
+}
